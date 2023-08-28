5409806ff4d114f90ff6fd4b01f2f283f6fac56c
Cfinch99 <cfinch21@student.cccs.edu>
2023-08-28 17:53:45 -0600

    attempted to add a line

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+
+    public static void main(String[] args) {
+        System.out.println("I am bad at programming")
+            }
         }
     }
 }
