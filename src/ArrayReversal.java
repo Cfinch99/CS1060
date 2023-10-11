9d5224575968c071d7f35bab826b8b0302f03197
Cfinch99 <cfinch21@student.cccs.edu>
2023-10-10 19:18:22 -0600

    First draft of array reversal assignment.

---
+++ src/ArrayReversal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ArrayReversal {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[100];
+        System.out.println("Enter elements for array: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+            if (a[i] == -1) {
+                break;
+            }
+        }
+        for (int i = a.length - 1; i > -1; i--) {
+            if (a[i] > 0) {
+                System.out.print(a[i] + " ");
+
+
+            }
+        }
+    }
+}
